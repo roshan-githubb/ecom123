9809405a279cfb05233db838d84153a22ff81b21
Roshan Budhathoki <roshanbc9860@gmail.com>
2026-01-14 02:00:31 +0545

    #WSM-I582-profile icon added in navbar

--- src/components/cells/Navbar/Navbar.tsx
+++ src/components/cells/Navbar/Navbar.tsx
@@ -264,9 +264,34 @@ const showFAQLabel = pathname == "/np/footer/faq"
             )}
           </div>
         </>
-        {/* Right: search + cart */}
+        {/* Right: search + profile*/}
         <div className="flex w-full justify-between md:justify-end lg:ml-auto items-center space-x-4">
           {showSearchbar && <NavbarSearch />}
+          
+          {/* Profile Icon */}
+          <LocalizedClientLink
+            href="/profile"
+            className="mt-1 p-2 rounded-full hover:bg-blue-600 transition-colors"
+            title="Profile"
+          >
+            <svg 
+              width="24" 
+              height="24" 
+              viewBox="0 0 24 24" 
+              fill="none" 
+              xmlns="http://www.w3.org/2000/svg"
+              className="text-white"
+            >
+              <path 
+                d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z" 
+                fill="currentColor"
+              />
+              <path 
+                d="M12 14C7.58172 14 4 17.5817 4 22H20C20 17.5817 16.4183 14 12 14Z" 
+                fill="currentColor"
+              />
+            </svg>
+          </LocalizedClientLink>
 
           {/* <CartButton totalItems={totalItems} goToCheckoutPage={goToCheckoutPage} /> */}
         </div>
