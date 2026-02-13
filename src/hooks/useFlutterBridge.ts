bd71d116ce77b3d7b6aa3b6ac71179272f889b56
Roshan Budhathoki <roshanbc9860@gmail.com>
2026-02-13 15:00:59 +0545

    #WSM-I720: Commented out CartIcon since its not used

--- src/hooks/useFlutterBridge.ts
+++ src/hooks/useFlutterBridge.ts
@@ -2,11 +2,11 @@ import { sendToFlutter } from "@/lib/bridge/mobileBridge";
 
 export function useFlutterBridge() {
     return {
-        goHome: () => sendToFlutter("home"),
-        goCategories: () => sendToFlutter("categories"),
-        goOffers: () => sendToFlutter("offers"),
-        goCheck: () => sendToFlutter("check"),
-        goProfile: () => sendToFlutter("profile"),
+        // goHome: () => sendToFlutter("home"),
+        // goCategories: () => sendToFlutter("categories"),
+        // goOffers: () => sendToFlutter("offers"),
+        // goCheck: () => sendToFlutter("check"), // Not used - CartButton is commented out in Navbar
+        // goProfile: () => sendToFlutter("profile"),
         exitWebView: () => sendToFlutter("exit"),
     };
 }
